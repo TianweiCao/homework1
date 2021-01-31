10dbb3d20eaf85981342f29d04aca8d198f98bf9
Tianwei Cao <78243340+TianweiCao@users.noreply.github.com>
2021-01-31 14:44:48 +0800

    Update unit.py

--- unit.py
+++ unit.py
@@ -3,38 +3,38 @@ def gram2kgram(a,b):
    
     return b/1000
   elif a=="kgram":
-    return b*1000
+    return b*1000 #missed
   else:
     return "please enter valid input"   
   
 
 def c2f(a,b):
   if a=="F":
-    return b/33.8
+    return b/33.8 #missed
   elif a=="C":
     return b*33.8
   else:
-    return "please enter valid input"
+    return "please enter valid input" #missed
   
 def meter2mile(a,b):
   if a=="meter":
-    return b/1609
+    return b/1609 #missed
   elif a=="mile":
     return b*1609
   else:
-    return "please enter valid input"
+    return "please enter valid input" #missed
   
 def meter2feet(a,b):
   if a=="meter":
     return b*3.2
-  elif a=="feet":
+  elif a=="feet": #missed
     return b/3.2
   else:
     return "please enter valid input"
 
 def meter2inch(a,b):
   if a=="meter":
-    return b*39.4
+    return b*39.4 #missed
   elif a=="inch":
     return b/39.4
   else:
@@ -48,5 +48,9 @@ def test_right():
     assert meter2feet("meter",1)==3.2
 def test_wrong():
     assert gram2kgram("gra",1000)== "please enter valid input" 
+    assert c2f("d",1)== "please enter valid input"
+    assert meter2mile("feet",1000)== "please enter valid input"
+    assert meter2feet("mile",1000)== "please enter valid input"
+    assert meter2inch("feet",1000)== "please enter valid input"
     
         
