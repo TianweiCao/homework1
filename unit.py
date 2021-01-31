e9113eb1bd6fb7271cf8aa5b454513839a81b1a1
Tianwei Cao <78243340+TianweiCao@users.noreply.github.com>
2021-01-31 10:14:04 +0800

    Update unit.py

--- unit.py
+++ unit.py
@@ -6,17 +6,8 @@ def gram2kgram(a,b):
   else:
     return "please enter valid input"   
   
-def c2f(a,b):
-  if a=="F":
-    return b/33.8
-  elif a=="C":
-    return b*33.8
-  else:
-    return "please enter valid input"
 
   def test_gram2kgram():
     assert gram2kgram("gram",1000)==1
     
-  def test_c2f():
-    assert c2f("C",1)==33.8
-     
+  
