2855625d01128673081b746d605104f59e149e2e
Tianwei Cao <78243340+TianweiCao@users.noreply.github.com>
2021-01-31 09:18:22 +0800

    Update unit.py

--- unit.py
+++ unit.py
@@ -1,10 +1,10 @@
-def unit(a,b)
+def unit(a,b):
   if a=1
     return b/1000
   eif a=2
     return b*1000
   else
     return "shit"   
-def test_unit()
+def test_unit():
   assert unit(1,1000)==1
      
