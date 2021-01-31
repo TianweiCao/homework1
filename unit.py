97971fa96cac9c5d7daf2119877c5845bb870e1a
Tianwei Cao <78243340+TianweiCao@users.noreply.github.com>
2021-01-31 09:08:08 +0800

    Create unit.py

---
+++ unit.py
@@ -0,0 +1,12 @@
+  def Converse(A,B)
+ 
+    if A=1
+     return B/1000
+    eif A=2
+     return B*1000
+    else
+     return "shit"
+     
+  def test_Converse()
+    assert Converse(1,1000)==1
+     
