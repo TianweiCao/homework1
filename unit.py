99f24a58785a57f4c2c582caed491f53f9f25032
Tianwei Cao <78243340+TianweiCao@users.noreply.github.com>
2021-01-31 09:48:35 +0800

    Update unit.py

--- unit.py
+++ unit.py
@@ -1,10 +1,13 @@
 def unit(a,b):
   if a=="meter":
     return b/1000
+  if a=="temperature"
+    return b*33.8
   elif a=="kilometer":
     return b*1000
   else:
     return "shit"   
+  
 def test_unit():
   assert unit("meter",1000)==1
      
