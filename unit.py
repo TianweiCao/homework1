03f3d292bc60ee9efe4f9d3427768dea5f206858
Tianwei Cao <78243340+TianweiCao@users.noreply.github.com>
2021-01-31 11:44:55 +0800

    Update unit.py

--- unit.py
+++ unit.py
@@ -1,8 +1,9 @@
-def gram2kgram(a,b):
-  if a=="gram":
-    return b/1000
-  elif a=="kgram":
-    return b*1000
+def gram2kgram(a,b,c):
+  if a=="gram",c=="kgram":
+   
+    return b/1000,c
+  elif a=="kgram",c=="gram":
+    return b*1000,c
   else:
     return "please enter valid input"   
   
@@ -27,7 +28,7 @@ def test_c2f():
     assert c2f("C",1)==33.8
      
 def test_c2f():
-    assert gram2kgram("gram",1000)==1
+    assert gram2kgram("gram",1000,"kgram")==1,"kgram"
 
 def test_meter2mile():
     assert meter2mile("mile",1)==1609
