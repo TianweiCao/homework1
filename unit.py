0a112cbfe38d0eaa8e62c46e3c04a60cfcecbefb
Tianwei Cao <78243340+TianweiCao@users.noreply.github.com>
2021-01-31 14:25:38 +0800

    Update unit.py

--- unit.py
+++ unit.py
@@ -3,7 +3,7 @@ def gram2kgram(a,b):
    
     return b/1000
   elif a=="kgram":
-    return str(float(b*1000))+"kgram"
+    return b*1000
   else:
     return "please enter valid input"   
   
@@ -23,11 +23,29 @@ def meter2mile(a,b):
     return b*1609
   else:
     return "please enter valid input"
+def meter2feet(a,b):
+  if a=="meter":
+    return b*3.2
+  elif a=="feet":
+    return b/3.2
+  else:
+    return "please enter valid input"
+
+ def meter2inch(a,b):
+  if a=="meter":
+    return b*39.4
+  elif a=="inch":
+    return b/39.4
+  else:
+    return "please enter valid input"
+
  
 def test_right():
     assert c2f("C",1)==33.8
-    assert gram2kgram("gram",1000)=="1"+"kgram"
+    assert gram2kgram("gram",1000)==1
     assert meter2mile("mile",1)==1609
+    assert meter2feet("meter",1)==3.2
 def test_wrong():
     assert gram2kgram("gra",1000)== "please enter valid input" 
+    
         
