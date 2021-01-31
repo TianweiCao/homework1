c52d4a348b7da889f0564d9daab9f673129e9913
Tianwei Cao <78243340+TianweiCao@users.noreply.github.com>
2021-01-31 09:14:38 +0800

    Update unit.py

--- unit.py
+++ unit.py
@@ -1,10 +1,10 @@
-def Converse(A,B)
+def unit(A,B)
   if A=1
     return B/1000
   eif A=2
     return B*1000
   else
     return "shit"   
-def test_Converse()
-  assert Converse(1,1000)==1
+def test_unit()
+  assert unit(1,1000)==1
      
